56fb1a27aefb0aec355c444a69dc2c2f9811a847
HP <HP@LAPTOP-ERKT2GQC>
2023-04-18 09:04:14 +0200

    Add project files.

---
+++ ProgPoe/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ProgPoe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Recipe recipe = new Recipe();
+            int[] ingr = new int[recipe.NumStep];
+
+
+            Console.WriteLine("Welcome to the Recipe Creator!\n" +
+                "Please indicate how many ingerd");
+            recipe.NumStep = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
---
+++ ProgPoe/Recipe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgPoe
+{
+    internal class Recipe
+    {
+        public string[] NameOfIn { get; set; }
+        public int[] QuantOfIn { get; set; }
+        public string[] UnitOfIn { get; set; }
+        public int NumIn;
+        public int NumStep;
+        public string[] Steps {get; set; }
+
+
+    }
+}
